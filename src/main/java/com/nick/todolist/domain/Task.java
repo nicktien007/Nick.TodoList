ff18dad5b7b85ca3fa1b4c4a3ac34ec98777afa3
nick <trojanhorse0077@gmail.com>
2020-05-15 01:59:02 +0800

    addTask

--- src/main/java/com/nick/todolist/domain/Task.java
+++ src/main/java/com/nick/todolist/domain/Task.java
@@ -3,9 +3,11 @@ package com.nick.todolist.domain;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import lombok.Data;
 import lombok.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.swing.text.html.Option;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -19,9 +21,10 @@ public class Task {
 
     private String description;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private Date deadLine;
 
-    private Date createDate;
+    private LocalDateTime createDate = LocalDateTime.now();
 
     private Boolean done;
 }
--- src/main/java/com/nick/todolist/web/TaskController.java
+++ src/main/java/com/nick/todolist/web/TaskController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -32,5 +33,17 @@ public class TaskController {
         model.addAttribute("task", task);
 
         return "detail";
+    }
+
+    @GetMapping("tasks/addTask")
+    public String addTask(){
+        return "addTask";
+    }
+
+    @PostMapping("/addTask")
+    public String addTask(Task task){
+        service.saveTask(task);
+
+        return "redirect:/";
     }
 }
