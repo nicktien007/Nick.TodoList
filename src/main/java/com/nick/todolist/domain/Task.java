fbae1397ad275231996bdc359267b00b143c42dd
nick <trojanhorse0077@gmail.com>
2020-05-23 02:17:46 +0800

    1.全部刪除新增確認功能 2.細節調整

--- src/main/java/com/nick/todolist/domain/Task.java
+++ src/main/java/com/nick/todolist/domain/Task.java
@@ -1,6 +1,11 @@
 package com.nick.todolist.domain;
 
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -8,20 +13,25 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
+@RequiredArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private String description;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     @Column(nullable = false)
     private LocalDateTime deadLine = LocalDateTime.now().plusDays(1);
 
-    private LocalDateTime createDate = LocalDateTime.now();
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createDate = LocalDateTime.now().plusDays(1);
 
     @Column(nullable = false)
     private Boolean done = false;
